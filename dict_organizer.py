7ffe64c651cbcbddddebcf486fd0a33c8b8d788b
janosspaits <140593800+janosspaits@users.noreply.github.com>
2023-12-09 11:52:06 +0000

    update dec9

--- dict_organizer.py
+++ dict_organizer.py
@@ -2,10 +2,10 @@ import json
 
 # swap json file name for the file that you want to order (desc)
 
-with open('three_point_counts.json', 'r') as file:
+with open('flopping_counts.json', 'r') as file:
     data = json.load(file)
 
 sorted_data = dict(sorted(data.items(), key=lambda item: item[1], reverse=True))
 
-with open('three_point_counts.json', 'w') as file:
+with open('flopping_counts.json', 'w') as file:
     json.dump(sorted_data, file, indent=4)
